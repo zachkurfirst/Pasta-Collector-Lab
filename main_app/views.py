474c8a587ed05e80290f0590094527010e200d49
Zach Kurfirst <zachmk11@gmail.com>
2024-01-09 17:51:53 -0500

    set up and render pastas index.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 
+pastas = [
+    {
+        'name': 'Bucatini',
+        'type': 'Ribbon-Cut',
+        'cook_time': 9
+    },
+    {
+        'name': 'Agnolotti',
+        'type': 'Stuffed',
+        'cook_time': 6
+    },
+    {
+        'name': 'Rigatoni',
+        'type': 'Tube-Shaped',
+        'cook_time': 8
+    }
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
+
+def pastas_index(request):
+    return render(request, 'pastas/index.html', {
+        'pastas': pastas,
+    })
